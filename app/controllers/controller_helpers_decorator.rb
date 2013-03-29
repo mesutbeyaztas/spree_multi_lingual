384d5b842cd3dd3ed9f104540e4f11d922fb1cb4
Aaron Cruz <aaron@aaroncruz.com>
2013-03-29 19:01:29 +0100

    Removed reference to Spree::Config.default_locale

--- app/controllers/controller_helpers_decorator.rb
+++ app/controllers/controller_helpers_decorator.rb
@@ -11,7 +11,7 @@ Spree::Core::ControllerHelpers.module_eval do
   # or Spree::Config[:default_locale]
   # or Rails.application.config.i18n.default_locale
   def set_user_language
-    locale = params[:locale] || session[:locale] || I18n.locale || Spree::Config[:default_locale] || Rails.application.config.i18n.default_locale
+    locale = params[:locale] || session[:locale] || I18n.locale || Rails.application.config.i18n.default_locale
     locale = I18n.default_locale unless locale && I18n.available_locales.include?(locale.to_sym)
     I18n.locale = locale.to_sym
   end
